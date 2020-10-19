b1816a350b1c50da2cf31f6ceae7c7eacb3869f9
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-10-19 21:33:45 +0900

    staff_editページ3

--- staff/staff_edit.php
+++ staff/staff_edit.php
@@ -46,5 +46,16 @@
   <form method="post" action="staff_edit_check.php">
   <input type="hidden" name="code" value="<?php print $staff_code;?>">
   スタッフ名<br/>
+  <input type="text" name="name" style="width:200px" value="<?php print $staff_name;?>"><br/>
+
+  パスワードを入力してください。<br/>
+  <input type="password" name="pass" style="width:100px"><br/>
+  パスワードをもう１度入力してください。<br/>
+  <input type="password" name="pass2" style="width:100px"><br/>
+  <br/>
+  <input type="button" onclick="history.back()" value="戻る">
+  <input type="submit" value="OK">
+  </form>
+  
 </body>
 </html>
