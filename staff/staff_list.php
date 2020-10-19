f78b590f214f976e9c246e8ad2ebcdf204c7b3d7
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-10-19 20:49:17 +0900

    staff_listページ3

--- staff/staff_list.php
+++ staff/staff_list.php
@@ -16,7 +16,7 @@
     $dbh=new PDO($dsn, $user, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql='SELECT name FROM mst_staff WHERE 1';
+    $sql='SELECT code,name FROM mst_staff WHERE 1';
     $stmt=$dbh->prepare($sql);
     $stmt->execute();
 
@@ -24,6 +24,7 @@
 
     print'スタッフ一覧<br/><br/>';
 
+    print'<form method="post"action="staff_edit.php">';
     while(true)
     {
       $rec=$stmt->fetch(PDO::FETCH_ASSOC);
@@ -31,9 +32,12 @@
       {
       break;
       }
+      print'<input type="radio"name="staffcode"value="value"'.$rec['code'].'">';
       print $rec['name'];
       print '<br/>';
     }
+    print'<input type="submit"value="修正">';
+    print'</form>';
   }
   catch(Exception $e)
   {
