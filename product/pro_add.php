41a14218c787582fc22094f8d8b8080e78b99ef3
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-10-23 22:14:02 +0900

    pro_addページ

---
+++ product/pro_add.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>shop_app</title>
+</head>
+<body>
+  商品追加<br/>
+  <br/>
+  <form method="post" action="pro_add_check.php">
+  商品名を入力してください。 <br/>
+  <input type="text" name="price" style="width:50px"><br/>
+  価格を入力してください。 <br/>
+  <input type="password" name="pass" style="width:100px"><br/>
+  <br/>
+  <input type="button" onclick="history.back()" value="戻る">
+  <input type="submit" value="OK">
+  </form>
+</body>
+</html>
