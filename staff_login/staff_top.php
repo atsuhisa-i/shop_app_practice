d54079d4e954ef9b841b9bea98894a6f9aa489b3
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-10-28 21:49:04 +0900

    staff_topページ
    staff_topページ作成に伴い、staff_list、pro_listページにもリンクを追記。

---
+++ staff_login/staff_top.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  ショップ管理トップメニュー<br/>
+  <br/>
+  <a href="../staff/staff_list.php">スタッフ管理</a><br/>
+  <br/>
+  <a href="../product/pro_list.php">商品管理</a><br/>
+</body>
+</html>
