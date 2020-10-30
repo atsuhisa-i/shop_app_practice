ddeeab6d1640f9ecbd886214e771ea4b316d36d2
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-10-30 22:10:53 +0900

    login session追記

--- staff/staff_ng.php
+++ staff/staff_ng.php
@@ -1,3 +1,19 @@
+<?php
+session_start();
+session_regenerate_id(true);
+if(isset($_SESSION['login'])==false)
+{
+  print'ログインされていません。<br/>';
+  print'<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+  exit();
+}
+else
+{
+  print $_SESSION['staff_name'];
+  print 'さんログイン中<br/>';
+  print '<br/>';
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,6 +25,6 @@
   
   スタッフが選択されていません。<br/>
   <a href="staff_list.php">戻る</a>
-  
+
 </body>
 </html>
