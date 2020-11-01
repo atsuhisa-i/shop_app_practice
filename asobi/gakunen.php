d9535b8f1e8b16d0438abfc97ce9e6cb08441081
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-11-01 14:14:15 +0900

    asobi4

--- asobi/gakunen.php
+++ asobi/gakunen.php
@@ -12,18 +12,31 @@
   switch($gakunen)
   {
     case'1':
-      print'あなたの校舎は南校舎です。';
+      $kousha='あなたの校舎は南校舎です。';
+      $bukatsu='部活動にはスポーツ系と文化系があります。';
+      $mokuhyou='まずは学校に慣れましょう。';
       break;
     case'2':
-      print'あなたの校舎は西校舎です。';
+      $kousha='あなたの校舎は西校舎です。';
+      $bukatsu='学園祭目指して全力で取り組みましょう。';
+      $mokuhyou='今しかできないことを見つけよう。';
       break;
     case'3':
-      print'あなたの校舎は東校舎です。';
+      $kousha='あなたの校舎は西校舎です。';
+      $bukatsu='受験に就業に忙しくなります。後輩に譲っていきましょう。';
+      $mokuhyou='将来への道を作ろう。';
       break;
     default:
-      print'あなたの校舎は3年生と同じです。';
+      $kousha='あなたの校舎は3年生と同じです。';
+      $bukatsu='部活動はありません。';
+      $mokuhyou='早く卒業しましょう。';
       break;
   }
+
+  print'校舎　'.$kousha.'<br/>';
+  print'部活　'.$bukatsu.'<br/>';
+  print'目標　'.$mokuhyou.'<br/>';
+  
   ?>
 </body>
 </html>
