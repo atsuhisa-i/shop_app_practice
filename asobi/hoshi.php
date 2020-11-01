eaa9c57d3ee5d2a2cb69b9986222df2cc8783334
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-11-01 13:24:02 +0900

    asobi1

---
+++ asobi/hoshi.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+
+  $mbango=$_POST['mbango'];
+
+  $hoshi['M1']='カニ星雲';
+  $hoshi['M31']='アンドロメダ大星雲';
+  $hoshi['M42']='オリオン大星雲';
+  $hoshi['M45']='すばる';
+  $hoshi['M57']='ドーナツ星雲';
+  
+  print'あなたが選んだ星は、';
+  print $hoshi[$mbango];
+
+  ?>
+</body>
+</html>
