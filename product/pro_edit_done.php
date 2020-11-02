d4e1e3d512ac1612f3f9797da214737ca77be656
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-11-02 21:23:45 +0900

    安全対策関数に書き換え

--- product/pro_edit_done.php
+++ product/pro_edit_done.php
@@ -25,16 +25,14 @@ else
   <?php
   try
   {
+    require_once('../common/common.php');
 
-    $pro_code=$_POST['code'];
-    $pro_name=$_POST['name'];
-    $pro_price=$_POST['price'];
+    $post=sanitize($_POST);
+    $pro_code=$post['code'];
+    $pro_name=$post['name'];
+    $pro_price=$post['price'];
     $pro_gazou_name_old=$_POST['gazou_name_old'];
     $pro_gazou_name=$_POST['gazou_name'];
-
-    $pro_code=htmlspecialchars($pro_code,ENT_QUOTES,'UTF-8');
-    $pro_name=htmlspecialchars($pro_name,ENT_QUOTES,'UTF-8');
-    $pro_price=htmlspecialchars($pro_price,ENT_QUOTES,'UTF-8');
 
     $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
     $user='root';
