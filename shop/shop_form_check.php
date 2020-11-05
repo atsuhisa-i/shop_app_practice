8ca53978bfe7444889bf83d741963e03dfe326ed
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-11-05 22:31:57 +0900

    shop_form_checkページ3

--- shop/shop_form_check.php
+++ shop/shop_form_check.php
@@ -24,26 +24,62 @@
   {
     print'お名前が入力されていません。<br/><br/>';
   }
+  else
+  {
+    print 'お名前<br/>';
+    print $onamae;
+    print '<br/><br/>';
+  }
 
   if(preg_match('/\A[\w\\.]+\@[\w\\.]+\.([a-z]+)\z/',$email)==0)
   {
     print 'メールアドレスを正確に入力してください。<br/><br/>';
   }
+  else
+  {
+    print 'メールアドレス<br/>';
+    print $email;
+    print '<br/><br/>';
+  }
+
   if(preg_match('/\A[0-9]+\z/',$postal1)==0)
   {
     print'郵便番号は半角数字で入力してください。<br/><br/>';
   }
+  else
+  {
+    print '郵便番号<br/>';
+    print $postal1;
+    print '-';
+    print $postal2;
+    print '<br/><br/>';
+  }
+
   if(preg_match('/\A[0-9]+\z/',$postal2)==0)
   {
     print'郵便番号は半角数字で入力してください。<br/><br/>';
   }
-  if($address='')
+
+  if($address=='')
   {
     print'住所が入力されていません。<br/><br/>';
   }
-  if(preg_match('/\A\d[2,5]-?\d[2,5]-?\d[4,5]\z/',$tel)==0)
+  else
+  {
+    print '住所<br/>';
+    print $address;
+    print '<br/><br/>';
+  }
+
+  if(preg_match('/\A\d{2,5}-?\d{2,5}-?\d{4,5}\z/',$tel)==0)
   {
     print'電話番号を正確に入力してください。<br/><br/>';
+  }
+  else
+  {
+    print '電話番号<br/>';
+    print $tel;
+    print '<br/><br/>';
   }
 
   print '<form method="post" action="shop_form_done.php">';
@@ -54,8 +90,9 @@
   print '<input type="hidden" name="address" value="'.$address.'">';
   print '<input type="hidden" name="tel" value="'.$tel.'">';
   print '<input type="button" onclick="history.back()" value="戻る">';
+  print '<input type="submit" value="OK"><br/>';
   print '</form>';
-  
+
 ?>
 </body>
 </html>
